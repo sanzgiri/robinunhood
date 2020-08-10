240c5f04be194f3da896e6d3ec70dd22e422be05
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-08-09 22:36:43 -0700

    first version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ashutosh Sanzgiri',                   # Type in your name
   author_email = 'sanzgiri@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sanzgiri/robinhood',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sanzgiri/robinunhood/archive/v_001.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sanzgiri/robinunhood/archive/0.0.1.tar.gz',    # I explain this later on
   keywords = ['robinhood', 'stock lists', 'collections'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
