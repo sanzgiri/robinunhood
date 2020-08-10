491de111f2aedb4fe678ca090168acb883e832df
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-08-09 22:40:51 -0700

    first version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'robinunhood',         # How you named your package folder (MyLib)
   packages = ['robinunhood'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'utilities for working with the Robinhood stock trading platform',   # Give a short description about your library
   author = 'Ashutosh Sanzgiri',                   # Type in your name
   author_email = 'sanzgiri@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sanzgiri/robinhood',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sanzgiri/robinunhood/archive/0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sanzgiri/robinunhood/archive/0.0.3.tar.gz',    # I explain this later on
   keywords = ['robinhood', 'stock lists', 'collections'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
