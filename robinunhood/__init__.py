3aeb7846d3030d0d3d1007c844a55b0160bc1e9e
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-09-08 21:57:14 -0700

    bump release

--- robinunhood/__init__.py
+++ robinunhood/__init__.py
@@ -1,3 +1,4 @@
-__version__ = "0.0.5"
+__version__ = "0.0.8"
 
-from robinunhood.collections import get_top_movers, get_most_popular, get_collection
+from robinunhood.rh_collections import get_top_movers, get_most_popular, get_collection, get_tipranks_sentiment
+from robinunhood.tipranks import get_tipranks_stocks, get_trending_stocks, get_news_sentiments, get_data
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'robinunhood',         # How you named your package folder (MyLib)
   packages = ['robinunhood'],   # Chose the same as "name"
-  version = '0.0.7',      # Start with a small number and increase it with every change you make
+  version = '0.0.8',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'utilities for working with the Robinhood stock trading platform',   # Give a short description about your library
   long_description = 'utilities for working with the Robinhood stock trading platform',
   author = 'Ashutosh Sanzgiri',                   # Type in your name
   author_email = 'sanzgiri@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sanzgiri/robinunhood',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sanzgiri/robinunhood/archive/0.0.7.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sanzgiri/robinunhood/archive/0.0.8.tar.gz',    # I explain this later on
   keywords = ['robinhood', 'stock lists', 'collections'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
